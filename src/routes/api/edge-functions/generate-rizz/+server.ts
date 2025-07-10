49fb067b185aa9dd2ed750ac6da9be8c300bfa4a
Ryan Roberts <ryanroberts562@gmail.com>
2025-07-10 12:12:05 +0300

    feat: almost with db saving

--- src/routes/api/edge-functions/generate-rizz/+server.ts
+++ src/routes/api/edge-functions/generate-rizz/+server.ts
@@ -1,4 +1,5 @@
 import { BlobStorageService } from "$lib/server/services/blob-storage.service";
+import { DatabaseService } from "$lib/server/services/database.service";
 import { GeminiService } from "$lib/server/services/gemini.service";
 import {
   jsonSuccessResponse,
@@ -9,18 +10,31 @@ import type { RequestHandler } from "./$types";
 
 export const POST = (async ({ request }) => {
   try {
+    // TODO: get user id from kinde auth and validate kinde access token
+
     const { blobUrl, formData } = (await request.json()) as {
       blobUrl: string;
       formData: RizzGPTFormData;
     };
 
     const file = await new BlobStorageService().downloadFileFromBlob(blobUrl);
-    const response = await new GeminiService().generateRizz({
+    const generateRizzResponse = await new GeminiService().generateRizz({
       rizzGPTFormData: formData,
       file,
     });
 
-    return jsonSuccessResponse(response);
+    const conversation = await new DatabaseService().createConversation({
+      // TODO: get user id from kinde auth
+      userId: "123",
+      rizzResponses: [generateRizzResponse.responses[0]],
+      rizzResponseDescription: generateRizzResponse.explanation,
+      initialUploadedConversationBlobUrl: blobUrl,
+      relationshipContext: formData,
+      matchName: generateRizzResponse.matchName,
+      status: "initial",
+    });
+
+    return jsonSuccessResponse(generateRizzResponse);
   } catch (error) {
     return unknownErrorResponse(error, "Processing failed");
   }
