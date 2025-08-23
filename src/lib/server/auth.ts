14e084c9984681ebf959b695cb2d77aaff3b00fc
Ryan Roberts <ryanroberts562@gmail.com>
2025-08-23 14:26:42 +0300

    feat: basic auth frameowkr

---
+++ src/lib/server/auth.ts
@@ -0,0 +1,20 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "./database/connection";
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "$env/static/private";
+
+export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      enabled: true,
+      clientId: GOOGLE_CLIENT_ID!,
+      clientSecret: GOOGLE_CLIENT_SECRET!,
+    },
+  },
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+});
---
+++ src/lib/server/trpc/routers/auth.router.ts
@@ -0,0 +1,28 @@
+import z from "zod/v4";
+import { publicProcedure, router } from "../t";
+import { auth } from "$lib/server/auth";
+
+export const authRouter = router({
+  signInWithEmail: publicProcedure
+    .input(z.object({ email: z.string(), password: z.string() }))
+    .mutation(async ({ input }) => {
+      return auth.api.signInEmail({
+        body: {
+          email: input.email,
+          password: input.password,
+        },
+        asResponse: true,
+      });
+    }),
+
+  signInSocial: publicProcedure
+    .input(z.object({ provider: z.string() }))
+    .mutation(async ({ input }) => {
+      return auth.api.signInSocial({
+        body: {
+          provider: input.provider,
+        },
+        asResponse: true,
+      });
+    }),
+});
