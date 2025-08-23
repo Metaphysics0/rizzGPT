b96a07e9b45e2ed91ff11bd05f92a13e77dda170
Ryan Roberts <ryanroberts562@gmail.com>
2025-08-23 12:56:26 +0300

    feat: restore layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
+  import { page } from "$app/state";
+  import Header from "$lib/ui/Layout/Header.svelte";
+  import { fade } from "svelte/transition";
   import "../app.css";
 
   let { children } = $props();
 </script>
 
-{@render children()}
+<div
+  class="min-h-screen bg-gradient-to-br from-pink-100 via-purple-50 to-yellow-100 p-4 md:p-8"
+>
+  <Header />
+  {#key page.url.pathname}
+    <div in:fade={{ duration: 100, delay: 100 }} out:fade={{ duration: 200 }}>
+      {@render children()}
+    </div>
+  {/key}
+</div>
