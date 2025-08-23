0c18244074bf03c1069bdf71c157df222cc28ffb
Ryan Roberts <ryanroberts562@gmail.com>
2025-08-23 18:41:38 +0300

    feat: better authing

--- src/lib/server/trpc/router.ts
+++
@@ -1,8 +0,0 @@
-import { router } from "./t";
-import { generateRizzRouter } from "./routers/generate-rizz.router";
-
-export const appRouter = router({
-  generateRizz: generateRizzRouter,
-});
-
-export type AppRouter = typeof appRouter;
