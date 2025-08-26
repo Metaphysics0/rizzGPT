3159afafaf8390ce2bd1498a7ec597c2f3db50f4
Ryan Roberts <ryanroberts562@gmail.com>
2025-08-26 23:10:02 +0300

    feat: proper origin url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,6 @@
 import { createAuthClient } from "better-auth/svelte";
+import { browser } from "$app/environment";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:5173",
+  baseURL: browser ? window.location.origin : undefined,
 });
