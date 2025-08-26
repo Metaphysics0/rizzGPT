8430ad443bbca536b707422c749040b83444b298
Ryan Roberts <ryanroberts562@gmail.com>
2025-08-26 23:32:58 +0300

    feat: clauud

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -8,9 +8,10 @@ import * as schema from "./database/schema";
 import { ResendService } from "./services/send-email.service";
 
 export const auth = betterAuth({
+  baseURL: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:5173",
   trustedOrigins: [
-    "http://localhost:5173", 
-    ...(process.env.VERCEL_URL ? [`https://${process.env.VERCEL_URL}`] : [])
+    "http://localhost:5173",
+    ...(process.env.VERCEL_URL ? [`https://${process.env.VERCEL_URL}`] : []),
   ],
   emailAndPassword: {
     enabled: true,
