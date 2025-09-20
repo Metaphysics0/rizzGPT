9e35816341531c9252e9a78f350a93fec61dcbe2
Ryan Roberts <ryanroberts562@gmail.com>
2025-09-20 11:52:24 +0300

    feat: update docs

--- src/lib/ui/Layout/Header.svelte
+++ src/lib/ui/Layout/Header.svelte
@@ -5,9 +5,11 @@
 <div class="mx-auto mb-9 mt-3 max-w-2xl text-center">
   <a
     href="/"
-    class="text-4xl md:text-5xl w-fit mx-auto z-50 flex items-center justify-center gap-1"
+    class="text-4xl md:text-5xl w-fit mx-auto z-50 flex items-center justify-center gap-1 mb-2"
   >
     <span> RizzGPT </span>
     <Icon icon="mingcute:ai-line" class="mb-0.5" stroke-width={0.25} />
   </a>
+
+  <p class="text-sm text-gray-500">Get an AI generated response in seconds!</p>
 </div>
