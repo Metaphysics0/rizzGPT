0f608e16b6c8e358969e044d2c65d0309b6dee97
Ryan Roberts <ryanroberts562@gmail.com>
2025-09-23 14:13:03 +0300

    feat: svg proper

--- src/lib/ui/landing-page/components/IPhoneAnimatedGif.svelte
+++ src/lib/ui/landing-page/components/IPhoneAnimatedGif.svelte
@@ -3,10 +3,12 @@
   import CircleAndEmojiSvg from "$lib/assets/ui-svgs/CircleAndEmojiSvg.svelte";
 </script>
 
-<div class="relative">
-  <CircleAndEmojiSvg className="absolute -right-11 top-11" />
-  <img src={IPhoneGif} alt="iphone-vid" class="relative z-10" />
-  <h4 class="pl-3 text-3xl font-bold w-[calc(100%-3rem)]">
+<div class="relative bottom-14 lg:bottom-0 h-[415px] lg:h-[600px]">
+  <img src={IPhoneGif} alt="iphone-vid" class="z-10 object-contain scale-90 lg:scale-100 absolute h-full w-full inset-0" />
+  <div class="relative top-24 lg:absolute lg:-right-14 xl:right-4 lg:top-10">
+    <CircleAndEmojiSvg />
+  </div>
+  <h4 class="w-[200px] md:w-[360px] mx-auto relative bottom-10 lg:bottom-28 left-10 md:left-32 lg:left-10 text-2xl lg:text-3xl font-bold">
     Upload their Bio &amp; Get suggestions for chats...
   </h4>
 </div>
