04fd823d30d252f9ee282fe4dee9b217943b641a
Ryan Roberts <ryanroberts562@gmail.com>
2025-09-23 12:35:39 +0300

    feat: basic hero section

---
+++ src/lib/ui/landing-page/components/DownloadAppButtons.svelte
@@ -0,0 +1,9 @@
+<script>
+  import AppStoreButton from "$lib/assets/app-store.svg";
+  import GooglePlayButton from "$lib/assets/google-play.svg";
+</script>
+
+<div class="flex items-center gap-2 h-12">
+  <img src={AppStoreButton} alt="app-store-button" class="h-full" />
+  <img src={GooglePlayButton} alt="google-play-store-button" class="h-full" />
+</div>
