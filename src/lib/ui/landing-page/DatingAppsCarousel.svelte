8cf8c5ec2897d3f29edde80b21834c6db1e4d8bb
Ryan Roberts <ryanroberts562@gmail.com>
2025-09-23 15:12:32 +0300

    feat: carousel dots

--- src/lib/ui/landing-page/DatingAppsCarousel.svelte
+++ src/lib/ui/landing-page/DatingAppsCarousel.svelte
@@ -20,7 +20,7 @@
   ];
 </script>
 
-<section class="py-20 bg-secondary">
+<section class="py-10 pt-15 bg-secondary">
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-shadow-lg">
     <div class="text-center mb-16">
       <h2 class="text-4xl font-bold text-white mb-4">
@@ -28,10 +28,10 @@
       </h2>
     </div>
 
-    <Carousel.Root>
-      <Carousel.Content>
+    <Carousel.Root opts={{ loop: false, containScroll: "trimSnaps" }}>
+      <Carousel.Content class="ml-0">
         {#each datingApps as { name, logo }}
-          <Carousel.Item class="basis-1/4">
+          <Carousel.Item class="basis-1/4 pl-2 pr-2 flex-shrink-0">
             <img
               src={logo}
               alt={name + " Logo"}
@@ -40,6 +40,7 @@
           </Carousel.Item>
         {/each}
       </Carousel.Content>
+      <Carousel.Dots class="mt-10" />
     </Carousel.Root>
   </div>
 </section>
--- src/lib/ui/landing-page/components/IPhoneAnimatedGif.svelte
+++ src/lib/ui/landing-page/components/IPhoneAnimatedGif.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import IPhoneGif from "$lib/assets/iphone-vid.gif";
   import CircleAndEmojiSvg from "$lib/assets/ui-svgs/CircleAndEmojiSvg.svelte";
+  import CircleAndEmojiSvgSmall from "$lib/assets/ui-svgs/CircleAndEmojiSvgSmall.svelte";
 </script>
 
 <div class="relative bottom-14 lg:bottom-0 h-[415px] lg:h-[600px]">
@@ -10,7 +11,10 @@
     class="z-10 object-contain scale-90 lg:scale-100 absolute h-full w-full inset-0"
   />
   <div class="relative top-24 lg:absolute lg:-right-14 xl:right-4 lg:top-10">
-    <CircleAndEmojiSvg />
+    <CircleAndEmojiSvgSmall />
+    <div class="hidden lg:block">
+      <CircleAndEmojiSvg />
+    </div>
   </div>
   <h4
     class="w-[200px] md:w-[360px] mx-auto absolute bottom-[-78px] left-24 text-2xl lg:text-3xl font-bold"
