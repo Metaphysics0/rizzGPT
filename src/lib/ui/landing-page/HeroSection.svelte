18adafe37b78ebf548f93b35f3f987def69b4ccd
Ryan Roberts <ryanroberts562@gmail.com>
2025-09-23 14:32:03 +0300

    feat: better vibes

--- src/lib/ui/landing-page/HeroSection.svelte
+++ src/lib/ui/landing-page/HeroSection.svelte
@@ -1,10 +1,13 @@
 <script>
   import DownloadAppButtons from "./components/DownloadAppButtons.svelte";
   import IPhoneAnimatedGif from "./components/IPhoneAnimatedGif.svelte";
+  import SatisfiedUsers from "./components/SatisfiedUsers.svelte";
   import TrustpilotScore from "./components/TrustpilotScore.svelte";
 </script>
 
-<section class="grid grid-cols-2 px-5 pt-10">
+<section
+  class="grid lg:grid-cols-2 lg:grid-rows-2 gap-x-4 gap-y-24 lg:gap-y-10 px-5 pt-10"
+>
   <div class="flex flex-col justify-center gap-4">
     <TrustpilotScore />
     <h4 class="text-6xl font-bold tracking-wide leading-20">
@@ -18,5 +21,8 @@
   </div>
   <div>
     <IPhoneAnimatedGif />
+  </div>
+  <div>
+    <SatisfiedUsers />
   </div>
 </section>
