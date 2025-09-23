d1ce5067818c711dad32a8b3645c7741f89bd1a9
Ryan Roberts <ryanroberts562@gmail.com>
2025-09-23 13:52:54 +0300

    feat: work on svg shiz

--- src/lib/ui/landing-page/components/IPhoneAnimatedGif.svelte
+++ src/lib/ui/landing-page/components/IPhoneAnimatedGif.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
   import IPhoneGif from "$lib/assets/iphone-vid.gif";
+  import CircleAndEmojiSvg from "$lib/assets/ui-svgs/CircleAndEmojiSvg.svelte";
 </script>
 
-<img src={IPhoneGif} alt="iphone-vid" />
+<div class="relative">
+  <CircleAndEmojiSvg className="absolute -right-11 top-11" />
+  <img src={IPhoneGif} alt="iphone-vid" class="relative z-10" />
+  <h4 class="pl-3 text-3xl font-bold w-[calc(100%-3rem)]">
+    Upload their Bio &amp; Get suggestions for chats...
+  </h4>
+</div>
