28b831b6e3c860f63f133e24990ad89e8d8ae45c
Ryan Roberts <ryanroberts562@gmail.com>
2025-09-23 16:27:46 +0300

    feat: add assets

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import LinkButton from "$lib/ui/general/LinkButton.svelte";
   import Testimonials from "$lib/ui/landing-page/components/Testimonials.svelte";
   import DatingAppsCarousel from "$lib/ui/landing-page/DatingAppsCarousel.svelte";
   import HeroSection from "$lib/ui/landing-page/HeroSection.svelte";
@@ -9,3 +10,7 @@
 <HeroSection />
 <DatingAppsCarousel />
 <Testimonials />
+
+<div class="flex justify-center">
+  <LinkButton label="Try it for free" href="/sign-in" />
+</div>
