9af370c00c28ef98f93a80cf8c6826a8afc14991
Ryan Roberts <ryanroberts562@gmail.com>
2025-09-23 11:17:42 +0300

    feat: basic navbar

--- src/lib/ui/layout/Logo.svelte
+++ src/lib/ui/layout/Logo.svelte
@@ -1,8 +1,11 @@
-<script>
+<script lang="ts">
+  import { cn } from "$lib/utils";
   import Icon from "@iconify/svelte";
+
+  let { className }: { className?: string } = $props();
 </script>
 
-<div class="flex flex-row gap-1">
+<div class={cn("flex flex-row gap-1 text-2xl", className)}>
   <span> RizzGPT </span>
   <Icon icon="mingcute:ai-line" class="mb-0.5" stroke-width={0.25} />
 </div>
--- src/lib/ui/layout/Navbar.svelte
+++ src/lib/ui/layout/Navbar.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import LinkButton from "../general/LinkButton.svelte";
   import Logo from "./Logo.svelte";
 
   const NAV_ITEMS = [
@@ -21,16 +22,17 @@
   ] as const;
 </script>
 
-<nav class="flex w-full justify-around">
+<nav class="flex w-full justify-around items-center">
   <Logo />
-  <div>
+
+  <div class="flex gap-10">
     {#each NAV_ITEMS as { href, label }}
-      <a {href}>{label}</a>
+      <a class="hover:text-primary duration-100" {href}>{label}</a>
     {/each}
   </div>
 
-  <div>
-    <button>Login</button>
-    <button>Get Started</button>
+  <div class="flex gap-5 items-center">
+    <a class="text-primary font-bold duration-100" href="/sign-in">Login</a>
+    <LinkButton label="Get Started" href="/sign-in" />
   </div>
 </nav>
