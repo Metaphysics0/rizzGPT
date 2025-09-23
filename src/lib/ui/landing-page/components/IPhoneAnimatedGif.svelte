3530974ec22fa9b585bec53f2dac4227bb48c82c
Ryan Roberts <ryanroberts562@gmail.com>
2025-09-23 14:32:03 +0300

    feat: better vibes

--- src/lib/ui/landing-page/components/IPhoneAnimatedGif.svelte
+++ src/lib/ui/landing-page/components/IPhoneAnimatedGif.svelte
@@ -4,11 +4,17 @@
 </script>
 
 <div class="relative bottom-14 lg:bottom-0 h-[415px] lg:h-[600px]">
-  <img src={IPhoneGif} alt="iphone-vid" class="z-10 object-contain scale-90 lg:scale-100 absolute h-full w-full inset-0" />
+  <img
+    src={IPhoneGif}
+    alt="iphone-vid"
+    class="z-10 object-contain scale-90 lg:scale-100 absolute h-full w-full inset-0"
+  />
   <div class="relative top-24 lg:absolute lg:-right-14 xl:right-4 lg:top-10">
     <CircleAndEmojiSvg />
   </div>
-  <h4 class="w-[200px] md:w-[360px] mx-auto relative bottom-10 lg:bottom-28 left-10 md:left-32 lg:left-10 text-2xl lg:text-3xl font-bold">
+  <h4
+    class="w-[200px] md:w-[360px] mx-auto absolute bottom-[-78px] lg:bottom-28 left-10 md:left-32 lg:left-10 text-2xl lg:text-3xl font-bold"
+  >
     Upload their Bio &amp; Get suggestions for chats...
   </h4>
 </div>
---
+++ src/lib/ui/landing-page/components/SatisfiedUsers.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  type Gender = "men" | "women";
+
+  const { amountOfUsersToDisplay = 5 }: { amountOfUsersToDisplay?: number } =
+    $props();
+
+  function generateRandomAvatars(count: number) {
+    return Array.from({ length: count }, (_, idx) => {
+      const gender: Gender = Math.random() > 0.5 ? "men" : "women";
+      const userNumber = Math.floor(Math.random() * 99) + 1; // 1-99 for more variety
+      return {
+        id: idx,
+        avatar: `https://randomuser.me/api/portraits/${gender}/${userNumber}.jpg`,
+        alt: `User ${gender.slice(0, -1)} avatar ${idx + 1}`,
+      };
+    });
+  }
+
+  const randomAvatars = generateRandomAvatars(amountOfUsersToDisplay);
+</script>
+
+<div
+  class="relative flex flex-col items-center justify-end gap-8 px-6 bg-primary text-center text-white rounded-t-full h-[310px] mt-auto w-[278px]"
+>
+  <div class="absolute -top-6 flex items-center">
+    {#each randomAvatars as user (user.id)}
+      <img
+        alt={user.alt}
+        loading="lazy"
+        width="62"
+        height="62"
+        decoding="async"
+        class="aspect-square rounded-full -ml-4 border border-white bg-black"
+        src={user.avatar}
+      />
+    {/each}
+    <div
+      class="aspect-square w-[62px] bg-black rounded-full -ml-4 flex items-center justify-center text-white text-xl"
+    >
+      +1000
+    </div>
+  </div>
+  <h5 class="text-3xl">Join 1000s of satisfied users ;)</h5>
+  <a
+    class="font-light border py-2 px-8 mb-10 rounded-full hover:text-primary hover:bg-white duration-300"
+    href="#review">See reviews</a
+  >
+</div>
