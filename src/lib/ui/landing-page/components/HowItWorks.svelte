c5a0e3a70f89ba37302ffe5e700ff5fdd3875ea9
Ryan Roberts <ryanroberts562@gmail.com>
2025-09-23 17:07:14 +0300

    feat: how it works section

---
+++ src/lib/ui/landing-page/components/HowItWorks.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import BackgroundSquares from '$lib/assets/ui-svgs/background-squares.svg';
+</script>
+
+<section
+  class="py-24 px-4 text-center bg-no-repeat bg-center bg-cover"
+  style="background-image: url({BackgroundSquares})"
+>
+  <div class="container mx-auto">
+    <h2 class="text-4xl font-bold mb-6">How it Works?</h2>
+    <p class="max-w-2xl mx-auto text-lg text-gray-600 leading-relaxed">
+      It's simple! When you match with someone on any dating app, just capture a screenshot of their
+      bio. Open RizzGPT, upload the screenshot, and let our AI work its charm!
+    </p>
+  </div>
+</section>
