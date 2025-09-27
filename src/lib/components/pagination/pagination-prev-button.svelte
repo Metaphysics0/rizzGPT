10dd69afef77ff45de0493982c70fa3473d7a313
Ryan Roberts <ryanroberts562@gmail.com>
2025-09-27 18:45:24 +0300

    feat: add pagination + toast

---
+++ src/lib/components/pagination/pagination-prev-button.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { Pagination as PaginationPrimitive } from "bits-ui";
+	import Icon from "@iconify/svelte";
+	import { buttonVariants } from "$lib/components/button/index.js";
+	import { cn } from "$lib/utils.js";
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		children,
+		...restProps
+	}: PaginationPrimitive.PrevButtonProps = $props();
+</script>
+
+{#snippet Fallback()}
+	<Icon icon="mingcute:left-line" class="size-4" />
+{/snippet}
+
+<PaginationPrimitive.PrevButton
+	bind:ref
+	aria-label="Go to previous page"
+	class={cn(
+		buttonVariants({
+			size: "default",
+			variant: "ghost",
+			class: "gap-1 px-2.5 sm:pl-2.5",
+		}),
+		className
+	)}
+	children={children || Fallback}
+	{...restProps}
+/>
