9c39acca3478dd98afad04a4efe0ef8aedc2596b
Ryan Roberts <ryanroberts562@gmail.com>
2025-09-27 09:05:01 +0300

    feat: paraglide v1

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
+import { paraglideVitePlugin } from "@inlang/paraglide-js";
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig(() => {
   return {
-    plugins: [sveltekit(), tailwindcss()],
+    plugins: [
+      paraglideVitePlugin({
+        project: "./project.inlang",
+        outdir: "./src/paraglide",
+        strategy: ["url", "cookie", "baseLocale"],
+      }),
+      sveltekit(),
+      tailwindcss(),
+    ],
     server: {
       allowedHosts: [
         "eager-weeks-teach.loca.lt", // replace with your localtunnel domain
