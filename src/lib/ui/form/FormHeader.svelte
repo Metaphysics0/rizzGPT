2b4b56ef673985fd92a89bfac0df3a722521dab9
Ryan Roberts <ryanroberts562@gmail.com>
2025-09-27 12:13:43 +0300

    feat: more ui

---
+++ src/lib/ui/form/FormHeader.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  let { header, subheader }: { header: string; subheader: string } = $props();
+</script>
+
+<h1 class="heading-secondary mb-2">{header}</h1>
+<p class="text-responsive-md">{subheader}</p>
