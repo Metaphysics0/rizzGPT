4c11bbd97de7e7045cb6970c1c0435e84af69240
Ryan Roberts <ryanroberts562@gmail.com>
2025-10-02 10:49:47 +0300

    feat: basic ui plan

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,10 @@
 import type { RequestEvent } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
+import { getPlans } from "$lib/server/utils/plans.util";
 
 export const load: LayoutServerLoad = async ({ locals }: RequestEvent) => {
   return {
     user: locals.user,
+    plans: getPlans(),
   };
 };
