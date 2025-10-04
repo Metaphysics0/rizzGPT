9de8c10acea1fbcf950729c004a76f9a40ee5653
Ryan Roberts <ryanroberts562@gmail.com>
2025-10-04 12:24:26 +0300

    feat: basic profile page

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -1,5 +1,24 @@
 import type { PageServerLoad } from "./$types";
+import { UsageService } from "$lib/server/services/usage.service";
+import { SubscriptionService } from "$lib/server/services/subscription.service";
 
 export const load: PageServerLoad = async ({ locals }) => {
-  // fetch user usage data here
+  if (!locals?.user) {
+    throw new Error("Unauthorized");
+  }
+
+  const usageService = new UsageService();
+  const subscriptionService = new SubscriptionService();
+
+  const usageCount = await usageService.getUserUsageCount(locals.user.id);
+  console.log("USAGE", usageCount);
+
+  const subscription = await subscriptionService.getActiveSubscription(
+    locals.user.email
+  );
+
+  return {
+    usageCount,
+    subscription,
+  };
 };
