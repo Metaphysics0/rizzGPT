616d833468cdb5dccd697c94834fa306cb1ccf83
Ryan Roberts <ryanroberts562@gmail.com>
2025-10-11 16:50:42 +0300

    feat: initial work

--- src/lib/runes/forms/profile-optimizer-form.svelte.ts
+++ src/lib/runes/forms/profile-optimizer-form.svelte.ts
@@ -1,30 +1,15 @@
 import { FormStateBaseClass } from "./form-state-base-class.svelte";
+import { ImageHandler } from "./image-handler.svelte";
 
 class ProfileOptimizerForm extends FormStateBaseClass {
-  public form = $state({
-    imageFileNames: [] as string[],
-  });
-  public canGenerate = $derived(
-    this.form.imageFileNames.length > 0 && !this.response?.loading
+  images = new ImageHandler(5);
+
+  canGenerate = $derived(
+    this.images.fileNames.length > 0 && !this.response?.loading
   );
 
-  addImageFileName(fileName: string) {
-    if (this.form.imageFileNames.length < 5) {
-      this.form.imageFileNames.push(fileName);
-    }
-  }
-
-  removeImageFileName(fileName: string) {
-    const index = this.form.imageFileNames.indexOf(fileName);
-    if (index > -1) {
-      this.form.imageFileNames.splice(index, 1);
-    }
-  }
-
   setFormData(formData: FormData) {
-    this.form.imageFileNames.forEach((fileName) => {
-      formData.append("imageFileNames", fileName);
-    });
+    this.images.appendToFormData(formData);
   }
 }
 
